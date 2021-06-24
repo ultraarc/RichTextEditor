20bdfcce71c647471d073e48c04d9ea4a80722fc
hujinchao <hujinchao@qiyi.com>
2021-06-24 18:05:46 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h2>富文本编辑器DEMO</h2>
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
+  </div>
+</template>
+
+<script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: "<p>Content of the editor.</p>",
+      editorConfig: {
+        // The configuration of the editor.
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
