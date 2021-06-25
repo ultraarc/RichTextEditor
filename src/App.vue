b4c603107169fa426b0d615e2dc935b2a61a1b35
hujinchao <hujinchao@qiyi.com>
2021-06-25 16:54:25 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,94 @@
       :editor="editor"
       v-model="editorData"
       :config="editorConfig"
+      @ready="onReady"
     ></ckeditor>
     <button class="save-button" @click="save">save</button>
   </div>
 </template>
 
 <script>
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import DecoupledEditor from "@ckeditor/ckeditor5-build-decoupled-document";
+// import Font from "@ckeditor/ckeditor5-font/src/font";
+// import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily";
+
 export default {
   name: "App",
   components: {},
   data() {
     return {
-      editor: ClassicEditor,
+      editor: DecoupledEditor,
       editorData: "<p>Content of the editor.</p>",
       editorConfig: {
-        // The configuration of the editor.
+        ckfinder: {
+          uploadUrl: "http://www.mypage.com/api/uploadckeditor",
+        },
+        toolbar: {
+          items: [
+            "heading",
+            "|",
+            "fontSize",
+            "fontFamily",
+            "fontColor",
+            "fontBackgroundColor",
+            "imageInsert",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "strikethrough",
+            "highlight",
+            "removeFormat",
+            "|",
+            "alignment",
+            "|",
+            "numberedList",
+            "bulletedList",
+            "|",
+            "indent",
+            "outdent",
+            "|",
+            "todoList",
+            "link",
+            "blockQuote",
+            "imageUpload",
+            "insertTable",
+            "mediaEmbed",
+            "|",
+            "undo",
+            "redo",
+            "CKFinder",
+          ],
+        },
+        language: "cs",
+        // image: {
+        //   toolbar: [
+        //     "imageTextAlternative",
+        //     "imageStyle:full",
+        //     "imageStyle:side",
+        //     "linkImage",
+        //   ],
+        // },
+        table: {
+          contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+        },
+        licenseKey: "",
       },
     };
   },
   methods: {
     save() {
       console.log(this.editorData);
+    },
+
+    onReady(editor) {
+      // Insert the toolbar before the editable area.
+      editor.ui
+        .getEditableElement()
+        .parentElement.insertBefore(
+          editor.ui.view.toolbar.element,
+          editor.ui.getEditableElement()
+        );
     },
   },
 };
